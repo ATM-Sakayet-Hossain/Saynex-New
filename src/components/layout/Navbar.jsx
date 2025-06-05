70c479e876290aa0ebd00ced2a084aed933ed87d
ATM-Sakayet-Hossain <mdshakhawat434@gmail.com>
2025-06-06 00:39:51 +0600

    login page

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <><div>Lorem ipsum dolor sit, amet consectetu</div></>
+    <><div>Lorem ipsum dolor sit, amet consectet</div></>
   )
 }
 
