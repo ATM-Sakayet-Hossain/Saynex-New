afc0d143e4ac5b383824721ff5f9ba8d766f41b8
ATM SAKAYET HOSSAIN <mdshakhawat434@gmail.com>
2025-07-27 20:38:09 +0600

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,32 @@ import React from 'react'
 import Chatlist from '../components/Chatlist'
 
 const Home = () => {
+//   let heading = document.querySelector("h1");
+// heading.innerHTML = "Any Numta Generator";
+// let input = document.querySelector(".input");
+// let submit_btn = document.querySelector(".submit");
+// let reset_btn = document.querySelector(".reset");
+// let resultDiv = document.querySelector(".result");
+
+// submit_btn.addEventListener("click", () => {
+//     let namtanumber = parseInt(input.value);
+//     let i = 1;
+//     if (isNaN(namtanumber)) {
+//         alert('Please enter a valid number.');
+//     } else {
+//         resultDiv.innerHTML = '';
+//         while (i <= 10) {
+//             resultDiv.innerHTML += `${namtanumber} x ${i} = ${namtanumber * i}<br>`;
+//             i++;
+//         }
+//     }
+// });
+
+// reset_btn.addEventListener("click", () => {
+//     input.value = '';
+//     resultDiv.innerHTML = '';
+//     console.log("Input reset");
+// });
   return (
     <div className='pl-27 bg-gray-100'>
       <Chatlist />
